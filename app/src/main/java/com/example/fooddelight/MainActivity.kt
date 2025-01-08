c7a76f4067bbdb51160e3a8cbe9ccd735bc4c547
Harpreetsharmavats <harpreetsharma902@gmail.com>
2025-01-08 20:04:49 +0530

    changes

--- app/src/main/java/com/example/fooddelight/MainActivity.kt
+++ app/src/main/java/com/example/fooddelight/MainActivity.kt
@@ -5,13 +5,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.fooddelight.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(binding.root)
+        var NavController = findNavController(R.id.fragmentContainerView8)
+        var bottomnav = findViewById<BottomNavigationView>(R.id.navigation)
+        bottomnav.setupWithNavController(NavController)
 
 
     }
+
 }

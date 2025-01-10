52ff7579660d399ec200196e07fac683924047f2
Harpreetsharmavats <harpreetsharma902@gmail.com>
2025-01-10 13:56:59 +0530

    changes

--- app/src/main/java/com/example/fooddelight/Adapters/CartAdapter.kt
+++ app/src/main/java/com/example/fooddelight/Adapters/CartAdapter.kt
@@ -9,9 +9,9 @@ class CartAdapter(
     private val cartitems: MutableList<String>,
     private val price: MutableList<String>,
     private val foodimage: MutableList<Int>
-) : RecyclerView.Adapter<CartAdapter.CartViewHolder>(){
+) : RecyclerView.Adapter<CartAdapter.CartViewHolder>() {
 
-    private val itemsQuantities = IntArray(cartitems.size){1}
+    private val itemsQuantities = IntArray(cartitems.size) { 1 }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -23,12 +23,13 @@ class CartAdapter(
     }
 
     override fun onBindViewHolder(holder: CartAdapter.CartViewHolder, position: Int) {
-holder.bind(position)
+        holder.bind(position)
     }
 
     override fun getItemCount(): Int = cartitems.size
 
-    inner class CartViewHolder(private val binding: CartviewcardBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class CartViewHolder(private val binding: CartviewcardBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun bind(position: Int) {
             binding.apply {
                 val quantity = itemsQuantities[position]
@@ -36,9 +37,42 @@ holder.bind(position)
                 cartprice.text = price[position]
                 cartfoodimage.setImageResource(foodimage[position])
                 cartquantity.text = quantity.toString()
-
+                minusbtn.setOnClickListener {
+                    decrementItems(position)
+                }
+                plusbtn.setOnClickListener {
+                    incrementItems(position)
+                }
+                deleteitems.setOnClickListener {
+                    val itemPostion = adapterPosition
+                    if (itemPostion != RecyclerView.NO_POSITION) {
+                        deleteItems(position)
+                    }
+                }
             }
 
+        }
+
+        fun decrementItems(position: Int) {
+            if (itemsQuantities[position] > 1) {
+                itemsQuantities[position]--
+                binding.cartquantity.text = itemsQuantities[position].toString()
+            }
+        }
+
+        fun incrementItems(position: Int) {
+            if (itemsQuantities[position] < 20) {
+                itemsQuantities[position]++
+                binding.cartquantity.text = itemsQuantities[position].toString()
+            }
+        }
+
+        fun deleteItems(position: Int) {
+            cartitems.removeAt(position)
+            price.removeAt(position)
+            foodimage.removeAt(position)
+            notifyItemRemoved(position)
+            notifyItemRangeChanged(position, cartitems.size)
         }
 
     }

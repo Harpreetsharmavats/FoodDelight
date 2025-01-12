4d9eb1716a80cefa9ae2319399b69f56ec6abd69
Harpreetsharmavats <harpreetsharma902@gmail.com>
2025-01-12 10:30:59 +0530

    changes

--- app/src/main/java/com/example/fooddelight/Fragments/SearchFragment.kt
+++ app/src/main/java/com/example/fooddelight/Fragments/SearchFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.fooddelight.Adapters.PopularAdapter
 import com.example.fooddelight.R
@@ -12,27 +13,87 @@ import com.example.fooddelight.databinding.FragmentSearchBinding
 
 
 class SearchFragment : Fragment() {
-   private lateinit var binding:FragmentSearchBinding
-private lateinit var adapter: PopularAdapter
+    private lateinit var binding: FragmentSearchBinding
+    private lateinit var adapter: PopularAdapter
+    private val foodName = listOf("Ice cream", "Soup", "Pasta", "Roll")
+    private val Price = listOf("$1", "$4", "$7", "$5")
+    private val foodImage =
+        listOf(R.drawable.menu3, R.drawable.menu4, R.drawable.menu5, R.drawable.menu6)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
     }
+
+    private val filterMenuFood = mutableListOf<String>()
+    private val filterMenuPrice = mutableListOf<String>()
+    private val filterMenuImage = mutableListOf<Int>()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentSearchBinding.inflate(inflater,container,false)
-        val foodName = listOf("Ice cream", "Soup", "Pasta", "Roll")
-        val Price = listOf("$1", "$4", "$7", "$5")
-        val foodImage =
-            listOf(R.drawable.menu3, R.drawable.menu4, R.drawable.menu5, R.drawable.menu6)
-        val adapter = PopularAdapter(ArrayList(foodName),ArrayList(Price),ArrayList(foodImage))
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+
+        adapter = PopularAdapter(filterMenuFood,filterMenuPrice,filterMenuImage)
         binding.searchrv.layoutManager = LinearLayoutManager(requireContext())
         binding.searchrv.adapter = adapter
+
+        //setup for searchview
+        setupSearchView()
+        //show menu items
+        showMenu()
         return binding.root
+    }
+
+    private fun showMenu() {
+        filterMenuFood.clear()
+        filterMenuPrice.clear()
+        filterMenuImage.clear()
+
+        filterMenuFood.addAll(foodName)
+        filterMenuPrice.addAll(Price)
+        filterMenuImage.addAll(foodImage)
+
+        adapter.notifyDataSetChanged()
+
+    }
+
+    private fun setupSearchView() {
+        binding.menusearchview.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+                filterMenuItems(query)
+                return TODO("Provide the return value")
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                filterMenuItems(newText)
+                return true
+            }
+
+
+        })
+    }
+
+    private fun filterMenuItems(query: String) {
+        filterMenuFood.clear()
+        filterMenuPrice.clear()
+        filterMenuImage.clear()
+
+        foodName.forEachIndexed { index, food ->
+            if (food.contains(query, ignoreCase = true)) {
+                filterMenuFood.add(food)
+                filterMenuPrice.add(Price[index])
+                filterMenuImage.add(foodImage[index])
+            }
+        }
+        adapter.notifyDataSetChanged()
+
     }
 
     companion object {

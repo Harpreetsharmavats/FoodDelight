1877ec6e1ccc9ab035cfea124bc2a10844c2d3b0
Harpreetsharmavats <harpreetsharma902@gmail.com>
2025-01-13 18:15:07 +0530

    changes

--- app/src/main/java/com/example/fooddelight/Adapters/NotificationAdapter.kt
+++ app/src/main/java/com/example/fooddelight/Adapters/NotificationAdapter.kt
@@ -1,4 +1,30 @@
 package com.example.fooddelight.Adapters
 
-class NotificationAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fooddelight.databinding.NotificationBinding
+
+class NotificationAdapter(private val notification : ArrayList<String>,private val notificationImage: ArrayList<Int>): RecyclerView.Adapter<NotificationAdapter.NotificationViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotificationViewHolder {
+        return NotificationViewHolder(NotificationBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+
+    }
+
+
+
+    override fun onBindViewHolder(holder: NotificationViewHolder, position: Int) {
+holder.bind(notification[position],notificationImage[position])    }
+
+    override fun getItemCount(): Int =notification.size
+
+    class NotificationViewHolder(private val binding:NotificationBinding):RecyclerView.ViewHolder(binding.root) {
+        fun bind(notification: String, image: Int) {
+            binding .notificationtext.text = notification
+            binding.notificationimage.setImageResource(image)
+        }
+
+    }
 }
--- app/src/main/java/com/example/fooddelight/Fragments/NotificationBottomSheet.kt
+++ app/src/main/java/com/example/fooddelight/Fragments/NotificationBottomSheet.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.fooddelight.Adapters.NotificationAdapter
 import com.example.fooddelight.R
 import com.example.fooddelight.databinding.FragmentNotificationBottomSheetBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class NotificationBottomSheet : Fragment() {
-   private lateinit var binding:FragmentNotificationBottomSheetBinding
-
+class NotificationBottomSheet : BottomSheetDialogFragment() {
+    private lateinit var binding: FragmentNotificationBottomSheetBinding
+    private lateinit var adapter: NotificationAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -20,7 +23,22 @@ class NotificationBottomSheet : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentNotificationBottomSheetBinding.inflate(inflater,container,false)
+
+        binding = FragmentNotificationBottomSheetBinding.inflate(inflater, container, false)
+        binding.imageButton.setOnClickListener {
+            dismiss()
+        }
+        val notification = arrayListOf(
+            "Your order has been Canceled Successfully",
+            "Order has been taken by the driver",
+            "Congrats Your Order Placed"
+        )
+        val notificationImage =
+            arrayListOf(R.drawable.sademoji, R.drawable.truck, R.drawable.congrats)
+        adapter = NotificationAdapter(ArrayList(notification), ArrayList(notificationImage))
+        binding.notificationrv.layoutManager = LinearLayoutManager(requireContext())
+        binding.notificationrv.adapter = adapter
+
         return binding.root
     }
 

bd7e92c567bc44fbf00e28190f1b519c7d401032
Harpreetsharmavats <harpreetsharma902@gmail.com>
2025-01-14 13:05:23 +0530

    changes

---
+++ app/src/main/java/com/example/fooddelight/DetailsActivity.kt
@@ -0,0 +1,18 @@
+package com.example.fooddelight
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.fooddelight.databinding.ActivityDetailsBinding
+
+class DetailsActivity : AppCompatActivity() {
+    private val binding: ActivityDetailsBinding by lazy {
+        ActivityDetailsBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}

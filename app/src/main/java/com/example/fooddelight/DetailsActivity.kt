e602312d769620515b1a23c8fa08ed65e29a19db
Harpreetsharmavats <harpreetsharma902@gmail.com>
2025-01-26 21:20:48 +0530

    changes

--- app/src/main/java/com/example/fooddelight/DetailsActivity.kt
+++ app/src/main/java/com/example/fooddelight/DetailsActivity.kt
@@ -2,29 +2,36 @@ package com.example.fooddelight
 
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.example.fooddelight.Models.CartItems
 import com.example.fooddelight.databinding.ActivityDetailsBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class DetailsActivity : AppCompatActivity() {
-    private  var foodName:String?= null
+    private var foodName: String? = null
+    private var foodPrice: String? = null
     private var foodImage: String? = null
     private var foodDescription: String? = null
     private var foodIngredient: String? = null
+    private lateinit var auth: FirebaseAuth
     private val binding: ActivityDetailsBinding by lazy {
         ActivityDetailsBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
+        auth = FirebaseAuth.getInstance()
         foodName = intent.getStringExtra("ItemName")
-        //foodPrice =intent.getStringExtra("ItemPrice")
+        foodPrice = intent.getStringExtra("ItemPrice")
         foodDescription = intent.getStringExtra("ItemDescription")
         foodIngredient = intent.getStringExtra("ItemIngredients")
         foodImage = intent.getStringExtra("ItemImage")
 
-        with(binding){
+        with(binding) {
             detailfoodname.text = foodName
             detaildescription.text = foodDescription
             detailingredients.text = foodIngredient
@@ -32,7 +39,32 @@ class DetailsActivity : AppCompatActivity() {
 
         }
         binding.detailbackbtn.setOnClickListener {
-           finish()
+            finish()
+        }
+        binding.detailsaddtocart.setOnClickListener {
+            addToCart()
+        }
+    }
+
+    private fun addToCart() {
+        val database = FirebaseDatabase.getInstance().reference
+        val userId = auth.currentUser?.uid ?: ""
+
+        val cartItem = CartItems(
+            foodName.toString(),
+            foodIngredient.toString(),
+            foodImage.toString(),
+            foodDescription.toString(),
+            foodPrice.toString(),
+            1
+        )
+        database.child("user").child(userId).child("cartItems").push().setValue(cartItem)
+            .addOnSuccessListener {
+                Toast.makeText(this, "Item added to cart successfully", Toast.LENGTH_SHORT).show()
+                finish()
+            }.addOnFailureListener {
+            Toast.makeText(this, "Failed add to cart ", Toast.LENGTH_SHORT).show()
+
         }
     }
 }
---
+++ app/src/main/java/com/example/fooddelight/Models/CartItems.kt
@@ -0,0 +1,8 @@
+package com.example.fooddelight.Models
+
+data class CartItems( val foodName: String? = null,
+                      val foodPrice: String? = null,
+                      val foodImage: String? = null,
+                      val foodDescription: String? = null,
+                      val foodIngredient: String? = null,
+                      val foodQuantity: Int? = null)
